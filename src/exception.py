c1cb0375a9b78f437bec4bffe3caab3805eaeadf
Sushant <sushantsatelkar@gmail.com>
2025-06-20 18:55:50 +0530

    logging and exception

---
+++ src/exception.py
@@ -0,0 +1,29 @@
+import sys
+import logging
+
+def error_message_detail(error,error_detail:sys):
+    _,_,exc_tb=error_detail.exc_info()      #execution info will give 3 imp info 
+                                            #amongst which last one is imp that gives info about the exception
+    file_name=exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name,exc_tb.tb_lineno,str(error))
+    
+    return error_message
+
+
+
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        #inheriting exception thats why super
+        super().__init__(error_message)
+        self.error_message=error_message_detail(error_message,error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+    
+
+        
+
+
+
---
+++ src/logger.py
@@ -0,0 +1,24 @@
+import logging
+import os
+from datetime import datetime
+
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" #test that says date time in all those terms
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE) #this will have logs and the log file 
+os.makedirs(logs_path,exist_ok=True) #even tho theres a file or folder keep on appending them whenever we want to use 
+
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
+
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+
+
+)
+
+
+
+
+
+
